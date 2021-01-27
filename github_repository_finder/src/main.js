c2f78881fb4a6a2b72462278e641f504734c00d1
Danilo Guedes <danilotiba@gmail.com>
2021-01-27 00:39:40 -0300

    adicionando o metodo de renderizaçao da lista de repo na tela

--- github_repository_finder/src/main.js
+++ github_repository_finder/src/main.js
@@ -10,6 +10,9 @@ class App{
         //recuperando o formulário do html
         this.formulario = document.querySelector('form');
 
+        //recuperando o elemento de lista do HTML <ul>
+        this.listaHtml = document.querySelector('.list-group');
+
         //chamando o método que recupera os dados do formulário
         this.registrarDados(); 
     }
@@ -29,16 +32,82 @@ class App{
         this.listaRepositorios.push({
 
             nome: "Nerd Fonts",
-            resumo: "Iconic font aggregator, collection, and patcher",
+            descricao: "Iconic font aggregator, collection, and patcher",
             imagem: "https://avatars0.githubusercontent.com/u/8083459?v=4",
             link: "http://github.com/ryanoasis/nerd-fonts"
 
         });
 
-        //Renderizar a tela
-        console.log(this.listaRepositorios);
+        //Rendenrizar a tela
+        this.renderizarLista();
+        // console.log(this.listaRepositorios);
 
     }
+
+    renderizarLista(){        
+
+        //limpando a tela <ul>
+        this.listaHtml.innerHTML = ""
+
+        //Percorrer toda a lista de repositórios e criar os elementos
+        this.listaRepositorios.forEach(repo => {
+
+        //<li>
+        let li = document.createElement('li');
+        li.setAttribute('class', 'list-group-item list-group-item-action');
+
+            //<strong>
+            let strong = document.createElement('strong');
+            let txtNome = document.createTextNode(repo.nome);
+            strong.appendChild(txtNome);
+
+            //<p>
+            let p = document.createElement('p');
+            txtDescricao = document.createTextNode(repo.descricao);
+            p.appendChild(txtDescricao);
+            
+            //<img>
+            let img = document.createElement('img');
+            img.setAttribute('src', repo.imagem);
+            img.setAttribute('alt', 'imagem do repositório');
+
+            //<a>
+            let a = document.createElement('a');
+            a.setAttribute('href', repo.link);
+            a.setAttribute('target', '_blank');
+            txtLink = document.createTextNode("Acessar")
+            a.appendChild(txtLink);
+        
+        //adicionando todos os elementos no <li>
+        li.appendChild(strong);
+        li.appendChild(p);
+        li.appendChild(img);
+        li.appendChild(a);
+
+        //adicionando o <li> no <ul>
+        this.listaHtml.appendChild(li);
+
+        /*  MODELO DA LISTA DO HTML
+
+            <li class="list-group-item list-group-item-action">
+            <strong>Nerd Fonts</strong>
+            <p>Iconic font aggregator, collection, and patcher</p>
+            <img src="https://avatars0.githubusercontent.com/u/8083459?v=4" alt="imagem do repositório">
+            <a href="http://github.com/ryanoasis/nerd-fonts" target="_blank">Acessar</a>
+            </li> 
+        */
+
+        //limpando o formulário após submeter
+        this.formulario.querySelector('input[id=repositorio]').value = "";
+
+        //colocando foco no form novamente
+        this.formulario.querySelector('input[id=repositorio]').focus();
+
+        });
+        
+    }
+
+        
 }
 
 new App();
